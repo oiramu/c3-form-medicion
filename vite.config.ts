001f16dd35a7be9e65ed2e8a776892766acc857c
Carlos Rodriguez <32423618+oiramu@users.noreply.github.com>
2025-11-10 16:05:19 -0500

    Deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
     return {
-      plugins: [react()],
+      base: '/c3-form-medicion/',
+      plugins: [react(), tailwindcss()],
     };
 });
